c3afc7f1b6417587c14ff367741c4e9b828a4436
donato <donald199958@gmail.com>
2019-11-22 21:00:25 +0100

    Field is Written

--- main.cpp
+++ main.cpp
@@ -6,10 +6,6 @@ using namespace std;
 
 int main() {
     Field *field = new Field(10, 10, 4);
-    field->print_types();
-    printf("\n");
-    field->print_values();
-    printf("\n");
     field->print();
     for(int j = 0; j < field->get_len_y(); j++){
         for(int i = 0; i < field->get_len_x(); i++){
